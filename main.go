40199c7e7f1b3211df5e0ce52b1650720fc84a41
Caleb Penning <calebpenning@mail.com>
2022-01-25 22:59:54 -0800

    added post route to create a book

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"log"	
 	"net/http"
-	// "math/rand"
-	// "strconv"
+	"math/rand"
+	"strconv"
 	"github.com/gorilla/mux"
 )
 
@@ -51,7 +51,12 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
 // Create book instance
 func createBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var book book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000)) // NOT SAFE METHOD FOR GENERATING RANDOM ID
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 // Update existing book
