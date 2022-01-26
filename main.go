56fa9b98a1fb592c3170eb0bfc67b68e1823e69e
Caleb Penning <calebpenning@mail.com>
2022-01-25 23:07:53 -0800

    added put route

--- main.go
+++ main.go
@@ -61,7 +61,20 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 
 // Update existing book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index + 1:]...)
+			var book book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = strconv.Itoa(rand.Intn(10000000))
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 // Delete existing book
